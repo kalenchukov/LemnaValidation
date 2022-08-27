e6fad87b7c4bcff17893567c20a63aa1acb765d8
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-08-27 15:10:52 +0300

    Добавлена аннотация @Digit

---
+++ src/test/java/dev/kalenchukov/lemna/validation/validators/DigitValidatorTest.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2022 Алексей Каленчуков
+ * GitHub: https://github.com/kalenchukov
+ * E-mail: mailto:aleksey.kalenchukov@yandex.ru
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.kalenchukov.lemna.validation.validators;
+
+import dev.kalenchukov.lemna.validation.Validating;
+import dev.kalenchukov.lemna.validation.Validation;
+import dev.kalenchukov.lemna.validation.Violating;
+import dev.kalenchukov.lemna.validation.constraints.Digit;
+import dev.kalenchukov.lemna.validation.constraints.Length;
+import dev.kalenchukov.lemna.validation.exceptions.UnsupportedFieldTypeException;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class DigitValidatorTest
+{
+	/**
+	 * Проверка с некорректным типом поля.
+	 */
+	@Test(expected = UnsupportedFieldTypeException.class)
+	public void TestValidNotCorrectFieldType()
+	{
+		class Experimental
+		{
+			@Digit(min = 3, max = 13)
+			private String digit = "123";
+		}
+
+		Validating validation = new Validation(new Experimental());
+		List<Violating> violation = validation.validate();
+	}
+
+	/**
+	 * Проверка со значением {@code null}.
+	 */
+	@Test
+	public void TestValidValueNull()
+	{
+		class Experimental
+		{
+			@Digit(min = 3, max = 13)
+			private Integer digit = null;
+		}
+
+		Validating validation = new Validation(new Experimental());
+		List<Violating> violation = validation.validate();
+
+		assertEquals(0, violation.size());
+	}
+
+	/**
+	 * Проверка с некорректным значением в поле.
+	 */
+	@Test
+	public void TestValidValueNotCorrectMin()
+	{
+		class Experimental
+		{
+			@Digit(min = 3, max = 13)
+			private Integer digit = 12;
+		}
+
+		Validating validation = new Validation(new Experimental());
+		List<Violating> violation = validation.validate();
+
+		assertEquals(1, violation.size());
+	}
+
+	/**
+	 * Проверка с некорректным значением в поле.
+	 */
+	@Test
+	public void TestValidValueNotCorrectMax()
+	{
+		class Experimental
+		{
+			@Digit(min = 3, max = 13)
+			private Long digit = 12345678901234L;
+		}
+
+		Validating validation = new Validation(new Experimental());
+		List<Violating> violation = validation.validate();
+
+		assertEquals(1, violation.size());
+	}
+
+	/**
+	 * Проверка с корректным значением в поле типа {@code Integer}.
+	 */
+	@Test
+	public void TestValidIntegerTypeValueCorrect()
+	{
+		class Experimental
+		{
+			@Digit(min = 3, max = 13)
+			private Integer digit = 12345;
+		}
+
+		Validating validation = new Validation(new Experimental());
+		List<Violating> violation = validation.validate();
+
+		assertEquals(0, violation.size());
+	}
+
+	/**
+	 * Проверка с корректным значением в поле типа {@code Long}.
+	 */
+	@Test
+	public void TestValidLongTypeValueCorrect()
+	{
+		class Experimental
+		{
+			@Digit(min = 3, max = 13)
+			private Long digit = 12345L;
+		}
+
+		Validating validation = new Validation(new Experimental());
+		List<Violating> violation = validation.validate();
+
+		assertEquals(0, violation.size());
+	}
+
+	/**
+	 * Проверка с корректным значением в поле типа {@code Short}.
+	 */
+	@Test
+	public void TestValidShortTypeValueCorrect()
+	{
+		class Experimental
+		{
+			@Digit(min = 3, max = 13)
+			private Short digit = 12345;
+		}
+
+		Validating validation = new Validation(new Experimental());
+		List<Violating> violation = validation.validate();
+
+		assertEquals(0, violation.size());
+	}
+
+	/**
+	 * Проверка с корректным значением в поле типа {@code Byte}.
+	 */
+	@Test
+	public void TestValidByteTypeValueCorrect()
+	{
+		class Experimental
+		{
+			@Digit(min = 3, max = 13)
+			private Byte digit = 123;
+		}
+
+		Validating validation = new Validation(new Experimental());
+		List<Violating> violation = validation.validate();
+
+		assertEquals(0, violation.size());
+	}
+}
