40b3d4666453425ed6ef434ac875e537ffeb0bb9
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2023-01-13 16:58:33 +0300

    Добавлена аннотация WeekOfMonth

---
+++ src/main/java/dev/kalenchukov/lemna/validation/validators/WeekOfMonthValidator.java
@@ -0,0 +1,215 @@
+/*
+ * Copyright 2022 Алексей Каленчуков
+ * GitHub: https://github.com/kalenchukov
+ * E-mail: mailto:aleksey.kalenchukov@yandex.ru
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.kalenchukov.lemna.validation.validators;
+
+import dev.kalenchukov.lemna.validation.Violating;
+import dev.kalenchukov.lemna.validation.Violation;
+import dev.kalenchukov.lemna.validation.constraints.WeekOfMonth;
+import dev.kalenchukov.lemna.validation.exceptions.UnsupportedFieldTypeException;
+import dev.kalenchukov.string.formatting.StringFormat;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.reflect.Field;
+import java.util.Locale;
+import java.util.Objects;
+
+/**
+ * Класс проверяющего для ограничения {@link WeekOfMonth}.
+ */
+public final class WeekOfMonthValidator extends AbstractValidator
+{
+	/**
+	 * @see AbstractValidator#AbstractValidator(Locale)
+	 */
+	public WeekOfMonthValidator(@NotNull final Locale locale)
+	{
+		super(Objects.requireNonNull(locale));
+	}
+
+	/**
+	 * @see Validator#valid(Field, Object)
+	 */
+	@Nullable
+	@Override
+	public Violating valid(@NotNull final Field field, @Nullable final Object value)
+	{
+		Objects.requireNonNull(field);
+
+		WeekOfMonth constraint = field.getDeclaredAnnotation(WeekOfMonth.class);
+
+		boolean valid = this.isValid(field, constraint, value);
+
+		if (!valid)
+		{
+			this.setParam("FIELD", field.getName());
+			this.setParam("MIN", String.valueOf(Long.valueOf(1)));
+			this.setParam("MAX", String.valueOf(Long.valueOf(4)));
+
+			return new Violation(
+				field.getName(),
+				this.getMessage(),
+				this.getParams()
+			);
+		}
+
+		return null;
+	}
+
+	/**
+	 * Проверяет корректность значения поля класса.
+	 *
+	 * @param field поле класса.
+	 * @param constraint проверяемое ограничение.
+	 * @param value значение поля класса.
+	 * @return {@code true}, если значение поля класса корректно, иначе {@code false}.
+	 * @throws UnsupportedFieldTypeException если тип поля класса не поддерживается данным ограничением.
+	 */
+	private boolean isValid(@NotNull final Field field, @NotNull final WeekOfMonth constraint, @Nullable final Object value)
+	{
+		Objects.requireNonNull(field);
+		Objects.requireNonNull(constraint);
+
+		if (value == null) {
+			return true;
+		}
+
+		if (value.getClass().equals(Byte.class))
+		{
+			return this.isValidByte(constraint, (Byte) value);
+		}
+		else if (value.getClass().equals(Long.class))
+		{
+			return this.isValidLong(constraint, (Long) value);
+		}
+		else if (value.getClass().equals(Integer.class))
+		{
+			return this.isValidInteger(constraint, (Integer) value);
+		}
+		else if (value.getClass().equals(Short.class))
+		{
+			return this.isValidShort(constraint, (Short) value);
+		}
+		else
+		{
+			throw new UnsupportedFieldTypeException(String.format(
+				this.localeExceptions.getString("20001"),
+				constraint.getClass().getSimpleName()
+			));
+		}
+	}
+
+	/**
+	 * Проверка значения поля класса типа {@code Byte}.
+	 *
+	 * @param constraint проверяемое ограничение.
+	 * @param value значение поля класса.
+	 * @return {@code true}, если значение поля корректно, иначе {@code false}.
+	 */
+	private boolean isValidByte(@NotNull final WeekOfMonth constraint, @NotNull final Byte value)
+	{
+		Objects.requireNonNull(constraint);
+		Objects.requireNonNull(value);
+
+		return this.isValidAbstract(constraint, Long.parseLong(String.valueOf(value)));
+	}
+
+	/**
+	 * Проверка значения поля класса типа {@code Short}.
+	 *
+	 * @param constraint проверяемое ограничение.
+	 * @param value значение поля класса.
+	 * @return {@code true}, если значение поля корректно, иначе {@code false}.
+	 */
+	private boolean isValidShort(@NotNull final WeekOfMonth constraint, @NotNull final Short value)
+	{
+		Objects.requireNonNull(constraint);
+		Objects.requireNonNull(value);
+
+		return this.isValidAbstract(constraint, Long.parseLong(String.valueOf(value)));
+	}
+
+	/**
+	 * Проверка значения поля класса типа {@code Integer}.
+	 *
+	 * @param constraint проверяемое ограничение.
+	 * @param value значение поля класса.
+	 * @return {@code true}, если значение поля корректно, иначе {@code false}.
+	 */
+	private boolean isValidInteger(@NotNull final WeekOfMonth constraint, @NotNull final Integer value)
+	{
+		Objects.requireNonNull(constraint);
+		Objects.requireNonNull(value);
+
+		return this.isValidAbstract(constraint, Long.parseLong(String.valueOf(value)));
+	}
+
+	/**
+	 * Проверка значения поля класса типа {@code Long}.
+	 *
+	 * @param constraint проверяемое ограничение.
+	 * @param value значение поля класса.
+	 * @return {@code true}, если значение поля корректно, иначе {@code false}.
+	 */
+	private boolean isValidLong(@NotNull final WeekOfMonth constraint, @NotNull final Long value)
+	{
+		Objects.requireNonNull(constraint);
+		Objects.requireNonNull(value);
+
+		return this.isValidAbstract(constraint, value);
+	}
+
+	/**
+	 * Проверка значения поля класса абстрактного типа.
+	 * В качестве абстракции используется тип {@code Long}.
+	 *
+	 * @param constraint проверяемое ограничение.
+	 * @param value значение поля класса.
+	 * @return {@code true}, если значение поля корректно, иначе {@code false}.
+	 */
+	private boolean isValidAbstract(@NotNull final WeekOfMonth constraint, @NotNull final Long value)
+	{
+		Objects.requireNonNull(constraint);
+		Objects.requireNonNull(value);
+
+		if (value < 1)
+		{
+			this.setMessage(StringFormat.format(
+				constraint.message(),
+				"DEFAULT_MESSAGE",
+				this.localeViolations.getString("90008")
+			));
+
+			return false;
+		}
+
+		if (value > 4)
+		{
+			this.setMessage(StringFormat.format(
+				constraint.message(),
+				"DEFAULT_MESSAGE",
+				this.localeViolations.getString("90009")
+			));
+
+			return false;
+		}
+
+		return true;
+	}
+}
---
+++ src/test/java/dev/kalenchukov/lemna/validation/validators/WeekOfMonthValidatorTest.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2022 Алексей Каленчуков
+ * GitHub: https://github.com/kalenchukov
+ * E-mail: mailto:aleksey.kalenchukov@yandex.ru
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.kalenchukov.lemna.validation.validators;
+
+import dev.kalenchukov.lemna.validation.Validating;
+import dev.kalenchukov.lemna.validation.Validation;
+import dev.kalenchukov.lemna.validation.Violating;
+import dev.kalenchukov.lemna.validation.constraints.WeekOfMonth;
+import dev.kalenchukov.lemna.validation.exceptions.UnsupportedFieldTypeException;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class WeekOfMonthValidatorTest
+{
+	/**
+	 * Проверка с некорректным типом поля.
+	 */
+	@Test(expected = UnsupportedFieldTypeException.class)
+	public void testValidNotCorrectFieldType()
+	{
+		class Experimental
+		{
+			@WeekOfMonth
+			private String weekOfMonth = "1";
+		}
+
+		Validating validation = new Validation(new Experimental());
+		List<Violating> violation = validation.validate();
+	}
+
+	/**
+	 * Проверка со значением {@code null}.
+	 */
+	@Test
+	public void testValidValueNull()
+	{
+		class Experimental
+		{
+			@WeekOfMonth
+			private Integer weekOfMonth = null;
+		}
+
+		Validating validation = new Validation(new Experimental());
+		List<Violating> violation = validation.validate();
+
+		assertEquals(0, violation.size());
+	}
+
+	/**
+	 * Проверка с некорректным значением.
+	 */
+	@Test
+	public void testValidValueNotCorrect()
+	{
+		class Experimental
+		{
+			@WeekOfMonth
+			private Short weekOfMonth = 5;
+		}
+
+		Validating validation = new Validation(new Experimental());
+		List<Violating> violation = validation.validate();
+
+		assertEquals(1, violation.size());
+	}
+
+	/**
+	 * Проверка с корректным значением в поле типа {@code Byte}.
+	 */
+	@Test
+	public void testValidByteTypeValueCorrect()
+	{
+		class Experimental
+		{
+			@WeekOfMonth
+			private Short weekOfMonth = 2;
+		}
+
+		Validating validation = new Validation(new Experimental());
+		List<Violating> violation = validation.validate();
+
+		assertEquals(0, violation.size());
+	}
+
+	/**
+	 * Проверка с корректным значением в поле типа {@code Short}.
+	 */
+	@Test
+	public void testValidShortTypeValueCorrect()
+	{
+		class Experimental
+		{
+			@WeekOfMonth
+			private Short weekOfMonth = 4;
+		}
+
+		Validating validation = new Validation(new Experimental());
+		List<Violating> violation = validation.validate();
+
+		assertEquals(0, violation.size());
+	}
+
+	/**
+	 * Проверка с корректным значением в поле типа {@code Integer}.
+	 */
+	@Test
+	public void testValidIntegerTypeValueCorrect()
+	{
+		class Experimental
+		{
+			@WeekOfMonth
+			private Integer weekOfMonth = 3;
+		}
+
+		Validating validation = new Validation(new Experimental());
+		List<Violating> violation = validation.validate();
+
+		assertEquals(0, violation.size());
+	}
+
+	/**
+	 * Проверка с корректным значением в поле типа {@code Long}.
+	 */
+	@Test
+	public void testValidLongTypeValueCorrect()
+	{
+		class Experimental
+		{
+			@WeekOfMonth
+			private Long weekOfMonth = 1L;
+		}
+
+		Validating validation = new Validation(new Experimental());
+		List<Violating> violation = validation.validate();
+
+		assertEquals(0, violation.size());
+	}
+}
